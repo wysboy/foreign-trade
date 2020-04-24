b6cf136c6e9506eaf252abb32f643167630a6a55
wangyisheng <294780655@qq.com>
2020-04-24 11:37:54 +0800

    第一次

---
+++ jeecg-boot-module-system/src/main/java/org/jeecg/JeecgApplication.java
@@ -0,0 +1,19 @@
+package org.jeecg;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Slf4j
+@EnableSwagger2
+@SpringBootApplication
+//@EnableAutoConfiguration
+public class JeecgApplication {
+
+  public static void main(String[] args)  {
+    ConfigurableApplicationContext application = SpringApplication.run(JeecgApplication.class, args);
+  }
+}
